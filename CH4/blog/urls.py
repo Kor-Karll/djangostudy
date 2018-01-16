104375ddcf142ac838604b05e1aeb5e346d42349
Kor-Karll <hankikab@gmail.com>
2018-01-16 11:47:47 +0900

    CH4 추가

---
+++ CH4/blog/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url
+from blog.views import *
+
+app_name='blog'
+
+urlpatterns = [
+    # Example: /
+    url(r'^$', PostLV.as_view(), name='index'),
+
+    # Example: /post/ (same as /)
+    url(r'^post/$', PostLV.as_view(), name='post_list'),
+
+    # Example: /post/django-example/
+    url(r'^post/(?P<slug>[-\w]+)/$', PostDV.as_view(), name='post_detail'),
+
+    # Example: /archive/
+    url(r'^archive/$', PostAV.as_view(), name='post_archive'),
+
+    # Example: /2012/
+    url(r'^(?P<year>\d{4})/$', PostYAV.as_view(), name='post_year_archive'),
+
+    # Example: /2012/nov/
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', PostMAV.as_view(), name='post_month_archive'),
+
+    # Example: /2012/nov/10/
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_day_archive'),
+
+    # Example: /today/
+    url(r'^today/$', PostTAV.as_view(), name='post_today_archive'),
+]
